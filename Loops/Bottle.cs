62584a4a7217f5e53e6b3c2a8f47feb0e7aa9ff9
Dmitry Miroshnichenko <diamoney@outlook.com>
2024-02-10 11:03:27 -0600

    add bottle.cs and starts classes

---
+++ Loops/Bottle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    public class Bottle
+    {
+        public int Volume {  get; set; }
+        public string Material {  get; set; }
+        public string FormFactor { get; set; }
+
+        public void Fall()
+        {
+            Console.WriteLine("Упс бутылка упала");
+        }
+
+        public void Razlojilas()
+        {
+            Console.WriteLine("Бутылка разложилась");
+        }
+    }
+
+    class Cup
+    {
+        int Volume { get; set; }
+        string Material { get; set; }
+        string Color { get; set; }
+
+        void Break()
+        {
+            Console.WriteLine("Упс стакан разбился");
+        }
+
+        void FillUp()
+        {
+            Console.WriteLine("Наполнил стакан");
+        }
+    }
+
+    class Car
+    {
+        string CarClass { get; set; }
+        int NumbersOfDoors {  get; set; }
+        bool IsCabriolet {  get; set; }
+
+        void StartEngine ()
+        {
+            Console.WriteLine("Врум врум");
+        }
+
+        void StopEngine()
+        {
+            Console.WriteLine("Заглушил двигатель");
+        }
+
+        void Drive()
+        {
+            Console.WriteLine("Машина едет");
+        }
+    }
+
+
+}
--- Loops/Program.cs
+++ Loops/Program.cs
@@ -4,125 +4,19 @@ namespace Loops
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            // for
-            Console.WriteLine("Задание с циклом for:" + "\n");
-            for (var i = 1; i <= 10; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine("Число " + i + " является четным");
-                }
-                else
-                {
-                    Console.WriteLine("Число " + i + " является не четным");
-                }
-            }
+            Console.WriteLine("Добро пожаловать в магазин");
+            Console.WriteLine("У нас есть бутылка");
+            
+            var bottle = new Bottle();
+            bottle.Volume = 100;
+            bottle.FormFactor = "плоская";
+            bottle.Material = "пластикова";
 
-            // while
-            Console.WriteLine("\n" + "\n" + "Задание с циклом while:" + "\n");
-            var count = 1;
-            while (count <= 10)
-            {
-                if (count % 3 == 0)
-                {
-                    Console.WriteLine("Число " + count + " кратно 3");
-                }
-                count++;
-            }
-
-            //do-while v1
-            Console.WriteLine("\n" + "\n" + "Задание с циклом do-while вариант 1:" + "\n");
-            int guess = 0;
-            do
-            {
-                while (true)
-                {
-                    Console.WriteLine("Введите число: ");
-                    var text = Console.ReadLine();
-                    if (int.TryParse(text, out guess))
-                    {
-                       break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
-                    }
-                }
-
-                if (guess == 5)
-                {
-                    Console.WriteLine("Угадали!");
-                }
-                else if (guess < 5)
-                {
-                    Console.WriteLine("Загаданное число больше");
-                }
-                else if (guess > 5)
-                {
-                    Console.WriteLine("Загаданное число меньше");
-                }
-            }
-            while (guess != 5);
-
-            //do-while v2
-            Console.WriteLine("\n" + "\n" + "Задание с циклом do-while вариант 2:" + "\n");
-            int guess2 = 0;
-            Random rnd = new Random();
-            var secret = rnd.Next(1, 10);
-            var c = 1;
-            Console.WriteLine("Я загадал число от 1 до 10" + "\n" + "попробуйте отгадать у вас 5 попыток.");
-            do
-            {
-                while (true)
-                {
-                    Console.WriteLine("Введите число: ");
-                    string text = Console.ReadLine();
-                    if (int.TryParse(text, out guess2))
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
-                    }
-                }
-                c++;
-                if (guess2 == secret)
-                {
-                    Console.WriteLine("Угадали!");
-                    break;
-                }
-                else if (guess2 < secret)
-                {
-                    Console.WriteLine("Загаданное число больше");
-                }
-                else if (guess2 > secret)
-                {
-                    Console.WriteLine("Загаданное число меньше");
-                }
-                if (c > 5)
-                {
-                    Console.WriteLine("Вы проиграли");
-                }
-            }
-            while (c <= 5);
-
-            // foreach
-            Console.WriteLine("\n" + "\n" + "Задание с циклом foreach:" + "\n");
-            int[] numbers = new int[5] { 10, 20, 30, 40, 50 };
-            foreach (var item in numbers)
-            {
-                if (item > 30)
-                { 
-                    Console.WriteLine("Элемент " + item + " больше 30"); 
-                }
-                else 
-                {
-                    Console.WriteLine("Элемент " + item + " меньше  или равен 30");
-                }
-            }
+            Console.WriteLine("Это бутылка " +  bottle.Material);
+            Console.WriteLine("Она " + bottle.FormFactor);
+            Console.WriteLine("Ее обьем " + bottle.Volume);
         }
     }
 }
